566a5642c74e92488bde24c2a0d139adc66d0acb
chae <corkdud@naver.com>
2022-10-11 12:34:41 +0900

    221011

---
+++ src/PortfolioData.js
@@ -0,0 +1,9 @@
+const PD = [
+    { id: 1, title: "pf01", color: ["#333", "#369", "#f00"], content: "first homepage", link: "Chae-G-0.github.io/test11", anchor: "pf01" },
+    { id: 2, title: "pf02", color: ["#ff0", "#ddd", "#fff"], content: "2th homepage", link: "Chae-G-0.github.io/test10", anchor: "pf02" },
+    { id: 3, title: "pf03", color: ["#333", "#369", "#f00"], content: "3th homepage", link: "Chae-G-0.github.io/test11", anchor: "pf03" },
+    { id: 4, title: "pf04", color: ["#333", "#369", "#f00"], content: "4th homepage", link: "Chae-G-0.github.io/test11", anchor: "pf04" },
+    { id: 5, title: "pf05", color: ["#333", "#369", "#f00"], content: "5th homepage", link: "Chae-G-0.github.io/test11", anchor: "pf05" },
+];
+
+export default PD;
